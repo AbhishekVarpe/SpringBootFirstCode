21c486101c1c49ca56191094db3599222b70c111
HP <HP@Abhiiii>
2025-06-05 11:39:53 +0530

    basic application

---
+++ src/main/java/com/project/SpringFirstDemoApplication.java
@@ -0,0 +1,74 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFirstDemoApplication.class, args);
+		System.out.println("hello abhishek");
+		
+		System.out.append("hello student");
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
